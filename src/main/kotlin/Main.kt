25ab15f528945e8070f5b8b35efb88b91cb9c613
rita khaseyi <ritakhaseyi@gmail.com>
2023-02-02 08:53:45 +0300

    STUDENT-RECORDS

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    var fullname = "ritaZippy"
+    var age: Byte = 127
+    var phone_number = 757770427
+    var weightInKg = 67
+    var iskenyan: Boolean = true
+    var notkenyan:Boolean = false
+
+
+    println()
+
+
+
+}
